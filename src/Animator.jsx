c2a48de6166f3aa225e860a975f40b5240499632
Brayden Stone <braydendstone@gmail.com>
2019-11-17 00:27:41 -0700

    initial changes, svg renders and can have the fill altered

---
+++ src/Animator.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactSVG from "react-svg";
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  svg: props => ({
+    backgroundColor: "#ddd",
+    '& path': {
+        fill: props.fillColor
+    }
+  })
+})
+
+const Animator = ({children, ...props}) => {
+
+  const classes = useStyles(props);
+
+  return (
+    <div className={classes.svg}>
+      <ReactSVG src={children} />
+    </div>
+  );
+};
+
+Animator.defaultProps = {
+    fillColor: 'red'
+}
+
+export default Animator;
